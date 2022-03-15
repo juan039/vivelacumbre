dfd2abe20531780c9deedacb03d358836f1f29aa
Erickvergara97 <81184269+Erickvergara97@users.noreply.github.com>
2022-03-15 18:02:11 -0500

    Feature/nav bar (#38)
    * nav bar mobile version
    
    * add new lines in .scss styles
    
    * close navbar onclick

---
+++ js/closeNavBar.js
@@ -0,0 +1,7 @@
+var navBar = document.getElementById('navbarList');
+var checkbox = document.getElementById('checkbox')
+
+
+navBar.addEventListener("click", function closeNavbar(){
+    checkbox.checked = false
+}) 
